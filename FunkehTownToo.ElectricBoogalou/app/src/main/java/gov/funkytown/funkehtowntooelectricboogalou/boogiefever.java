a5ed1fd5b8a0854f225d237b0413b9845ca9972e
brandonstrong <ultrabstrong@gmail.com>
2016-03-09 12:56:15 -0700

    A little more done

--- FunkehTownToo.ElectricBoogalou/app/src/main/java/gov/funkytown/funkehtowntooelectricboogalou/boogiefever.java
+++ FunkehTownToo.ElectricBoogalou/app/src/main/java/gov/funkytown/funkehtowntooelectricboogalou/boogiefever.java
@@ -1,13 +1,24 @@
 package gov.funkytown.funkehtowntooelectricboogalou;
 
+import android.content.Context;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+
+import java.io.IOException;
+import java.util.List;
 
 public class boogiefever extends AppCompatActivity {
 
@@ -26,6 +37,45 @@ public class boogiefever extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void actionevent(){
+        try
+        {
+            // Find all available drivers from attached devices.
+            UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+            List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
+            if (availableDrivers.isEmpty()) {
+                return;
+            }
+
+// Open a connection to the first available driver.
+            UsbSerialDriver driver = availableDrivers.get(0);
+            UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
+            if (connection == null) {
+                // You probably need to call UsbManager.requestPermission(driver.getDevice(), ..)
+                return;
+            }
+
+// Read some data! Most have just one port (port 0).
+            List<UsbSerialPort> ports = driver.getPorts();
+            UsbSerialPort port = ports.get(0);
+            port.open(connection);
+            try {
+                port.setBaudRate(115200);
+                byte buffer[] = new byte[16];
+                int numBytesRead = port.read(buffer, 1000);
+                Log.d(TAG, "Read " + numBytesRead + " bytes.");
+            } catch (IOException e) {
+                // Deal with error.
+            } finally {
+                port.close();
+            }
+        }
+        catch (Exception ex)
+        {
+            System.out.println("Dehr wahsh ahn ehrr");
+        }
     }
 
     @Override
